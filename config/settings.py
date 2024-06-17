e3436706f9192deb4a04b1a3fc152997c254bfb5
Kim Placha <Kimberly.Placha@sdgku.edu>
2024-06-17 12:53:38 -0700

    117-2

--- config/settings.py
+++ config/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+import certifi
+
+os.environ["SSL_CERT_FILE"] = certifi.where()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -122,3 +126,14 @@ STATICFILES_DIRS = [str(BASE_DIR.joinpath("static"))]
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
+EMAIL_HOST_USER = "kbisla94@gmail.com"
+EMAIL_HOST_PASSWORD = "caqqcfgkgdekjyls"
+
+# go back to https://myaccount.google.com/ >
+# app passwords > remove the password
--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,44 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import ContactForm
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
 
 
 # Create your views here.
+def home(request):
+    return render(request, 'pages/home.html')
+# Create your views here.
+
+
 def about(request):
     return render(request, 'pages/about.html')
+
+
+def contact(request):
+    # sourcery skip: extract-method, use-fstring-for-concatenation
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+
+        if form.is_valid():
+            # send email
+            email_to = "kimplacha@outlook.com"
+            email_from = form.cleaned_data["email"]
+            name = form.cleaned_data["name"]
+            message = form.cleaned_data["message"]
+
+            html = render_to_string("pages/email.html", request.POST)
+
+            send_mail(
+                "Message from " + name,
+                message,
+                email_from,
+                [email_to],
+                html_message=html
+            )
+
+            return redirect("home")
+
+    else:
+        form = ContactForm()
+
+    return render(request, 'pages/contact.html', {'form': form})
